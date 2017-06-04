bfdc4de13faf8e3e44be885b617ed0ef8f2f15f7
Egor Chebotarev <sekt88@gmail.com>
2017-06-04 15:32:00 +0300

    r5

--- app/src/main/java/com/example/coolerby/git5/MainActivity.java
+++ app/src/main/java/com/example/coolerby/git5/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 //rebase2
     //rebase3
+    //rebase4
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
